164e1637bdc5d60534d8f5f171f82ccc7b30e8d4
Isse S.Ibrahim <65451035+isibrahiim@users.noreply.github.com>
2021-06-25 15:58:13 +0300

    Delete clocl.js

--- clocl.js
+++
@@ -1,9 +0,0 @@
-function updateClock() {
-  document.querySelector(".clock").style.color = "#fff";
-  const clockContainer = document.querySelector(".clock");
-
-  clockContainer.innerHTML =
-    // "<b>" + new Date().toLocaleDateString("uk") + "</b>";
-    "<b>" + new Date().toLocaleTimeString("uk") + "</b>";
-}
-setInterval(updateClock, 1000);
