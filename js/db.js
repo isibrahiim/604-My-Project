20c6b67c7f8a3a8deb7cb2a90436681c163a7299
isse salad <you@example.com>
2021-06-25 15:58:58 +0300

    adding js file

--- js/db.js
+++ js/db.js
@@ -1,3 +1,14 @@
+// Display Time function
+function updateClock() {
+  document.querySelector(".clock").style.color = "#fff";
+  const clockContainer = document.querySelector(".clock");
+
+  clockContainer.innerHTML =
+    // "<b>" + new Date().toLocaleDateString("uk") + "</b>";
+    "<b>" + new Date().toLocaleTimeString("uk") + "</b>";
+}
+setInterval(updateClock, 1000);
+// JSON Data to HTML 
 const bio = {
   "title": "What I do",
   "btm-line" : '',
